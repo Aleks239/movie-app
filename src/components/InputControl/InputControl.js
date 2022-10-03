a40a67febfe73d21c5e94c12a50768b3e23d96b7
Aleksandr Romanov <alexromanov239@gmail.com>
2022-10-03 20:30:38 +0200

    change css classes

--- src/components/InputControl/InputControl.js
+++ src/components/InputControl/InputControl.js
@@ -5,14 +5,14 @@ function InputControl({handleFilter, movies, handleSearch}) {
   return (
    
  <Navbar className="custom-nav" bg="light" variant="light">
-            <Nav.Item className="custom-cont-one">
+            <Nav.Item className="custom-control-select">
             <Form.Select disabled={movies ? false : true} onChange={handleFilter}>
         <option>Sort By...</option>
         <option value="year">Year</option>
         <option value="episode">Episode</option>
       </Form.Select>
             </Nav.Item>
-           <Nav.Item className="custom-cont">
+           <Nav.Item className="custom-control-input">
            <Form.Control onChange={handleSearch} disabled={movies ? false : true} type="text" placeholder="Type to search..." />
            </Nav.Item>
       </Navbar>
